7406efe7307f7d830831b30cc8f0acc2d245972f
Hrushiingole <142684900+Hrushiingole@users.noreply.github.com>
2025-03-24 00:43:39 +0530

    getAllreviews done.

--- src/main/java/com/project/JobApplication/Reviews/ReviewController.java
+++ src/main/java/com/project/JobApplication/Reviews/ReviewController.java
@@ -1,8 +1,13 @@
 package com.project.JobApplication.Reviews;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/companies/{companyId}")
@@ -10,5 +15,13 @@ public class ReviewController {
 
     @Autowired
     private ReviewService reviewService;
+
+    @GetMapping("/reviews")
+    public ResponseEntity<List<Review>> getAllReviews(@PathVariable Long companyId) {
+        List<Review> reviews = reviewService.getAllReviews(companyId);
+        return ResponseEntity.ok(reviews);
+    }
+
+
 
 }
